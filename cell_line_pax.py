5aed62c9dd23be6b9ab6340c351b9396c1c5f940
zhouyang.lian <zhouyang.lian@familian.life>
2019-10-15 09:50:52 -0400

    initial commit

---
+++ cell_line_pax.py
@@ -0,0 +1,64 @@
+from datanator_query_python.query import query_pax
+from datanator_query_python.config import config as config_query
+import pandas as pd
+
+
+def organize_observation(observations, existing_dict):
+    """reorganize observation 
+    
+    Args:
+        observations (:obj:`list` of :obj:`dict`): list of observations
+        existing_dict (:obj:`dict`): exisiting dictionary of abundances
+
+    Returns:
+        (:obj:`list` of :obj:`dict`): list of reorged observations
+    """
+    try:
+        count = len(list(existing_dict.values())[0])
+    except IndexError:
+        count = 0
+    existing_keys = set(existing_dict.keys())
+    processed_keys = set()
+    for obs in observations:
+        if obs.get('protein_id') is not None:
+            uniprot_id = obs['protein_id']['uniprot_id']
+        else:
+            continue
+        abundance = float(obs['abundance'])
+        exisitng_abundances = existing_dict.get(uniprot_id)
+        if exisitng_abundances is None:
+            tmp = [None] * count
+            tmp.append(abundance)
+            existing_dict[uniprot_id] = tmp
+            processed_keys.add(uniprot_id)
+        else:
+            exisitng_abundances.append(abundance)
+            processed_keys.add(uniprot_id)
+    unprocessed_keys = existing_keys.difference(processed_keys)
+    for key in unprocessed_keys:
+        existing_dict[key].append(None)
+    return existing_dict 
+
+
+
+def main():
+    db = 'datanator'
+    conf = config_query.TestConfig()
+    username = conf.MONGO_TEST_USERNAME
+    password = conf.MONGO_TEST_PASSWORD
+    MongoDB = conf.SERVER
+    pax_manager = query_pax.QueryPax(MongoDB=MongoDB, db=db,
+                 verbose=True, max_entries=20, username = username, password = password)
+    publication = 'http://www.mcponline.org/cgi/doi/10.1074/mcp.M111.014050'
+    files, _ = pax_manager.get_file_by_publication(publication, projection={'_id': 0, 'file_name': 1, 'observation': 1})
+    file_name = []
+    prev = {}
+    for file in files:
+        file_name.append(file['file_name'])
+        cur = organize_observation(file['observation'], prev)
+        prev = cur
+    df = pd.DataFrame.from_dict(cur, orient='index', columns=file_name)
+    df.to_csv('~/karr_lab/10.1074.mcp.M111.014050.csv', header=True)
+
+if __name__ == "__main__":
+    main()
---
+++ tests/test_cell_line_pax.py
@@ -0,0 +1,31 @@
+import unittest
+import cell_line_pax
+
+
+class TestCellLinePax(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        cls.src = cell_line_pax
+    
+    def test_organize_observation(self):
+        input_0 = [{'protein_id':{'uniprot_id': 'id_0'}, 'abundance': '0.0'},
+                   {'protein_id':{'uniprot_id': 'id_1'}, 'abundance': '1.0'},
+                   {'protein_id':{'uniprot_id': 'id_2'}, 'abundance': '2.0'}]
+        exp_0 = {'id_0': [0.0], 'id_1': [1.0], 'id_2': [2.0]}
+        result_0 = self.src.organize_observation(input_0, {})
+        self.assertEqual(exp_0, result_0)
+        input_1 = [{'protein_id':{'uniprot_id': 'id_0'}, 'abundance': '1.0'},
+                   {'protein_id':{'uniprot_id': 'id_1'}, 'abundance': '2.0'},
+                   {'protein_id':{'uniprot_id': 'id_3'}, 'abundance': '2.0'}]
+        exp_1 = {'id_0': [0.0, 1.0], 'id_1': [1.0, 2.0], 'id_2': [2.0, None], 'id_3': [None, 2.0]}
+        result_1 = self.src.organize_observation(input_1, exp_0)
+        self.assertEqual(exp_1, result_1)
+        input_2 = [{'protein_id':{'uniprot_id': 'id_4'}, 'abundance': '1.0'},
+                   {'protein_id':{'uniprot_id': 'id_5'}, 'abundance': '2.0'},
+                   {'protein_id':{'uniprot_id': 'id_6'}, 'abundance': '2.0'}]
+        exp_2 = {'id_0': [0.0, 1.0, None], 'id_1': [1.0, 2.0, None], 
+                'id_2': [2.0, None, None], 'id_3': [None, 2.0, None],
+                'id_4': [None, None, 1.0], 'id_5': [None, None, 2.0], 'id_6': [None, None, 2.0]}
+        result_2 = self.src.organize_observation(input_2, exp_1)
+        self.assertEqual(exp_2, result_2)
